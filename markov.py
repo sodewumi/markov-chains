e05dd5771bccf81a64b9ea8a966d8526c48d502a
Hackbright Student <info@hackbrightacademy.com>
2015-04-14 12:21:53 -0700

    Completed the make_chains function

--- markov.py
+++ markov.py
@@ -1,14 +1,28 @@
 from sys import argv
 
-script, corpus = argv
+script, corpus_path = argv
 
-def make_chains(corpus):
+def make_chains(corpus_path):
     """Takes input text as string; returns dictionary of markov chains."""
-    corpus_txt = open(corpus)
-    for line in corpus_txt:
-        line = line.rstrip()
-        print line
+    corpus_txt = open(corpus_path)
+    bi_grams = {}
+    
+    text = corpus_txt.read().split()
+
+    for wrd in range(len(text)-2):
+        key = (text[wrd], text[wrd +1])
+        nxt_wrd = text[wrd + 2]
+
+        if key not in bi_grams.keys():
+            bi_grams[key] = [nxt_wrd]
+        else:
+            bi_grams[key].append(nxt_wrd)
+
+        # Another way to preform the if else statement above
+        # bi_grams[key] = bigrams.get(key, []).append(nxt_wrd)
+
     # return {}
+    return bi_grams
 
 
 def make_text(chains):
@@ -24,7 +38,8 @@ def make_text(chains):
 input_text = "Some text"
 
 # Get a Markov chain
-chain_dict = make_chains(corpus)
+chain_dict = make_chains(corpus_path)
+print(chain_dict)
 
 # Produce random text
 random_text = make_text(chain_dict)
