671c1254ea42337079434733d71ad68e4b779c3a
Hackbright Student <info@hackbrightacademy.com>
2015-04-14 13:37:10 -0700

    Added comments and fixed bug

--- markov.py
+++ markov.py
@@ -32,28 +32,39 @@ def make_text(chains):
     nxt_wrd = None
 
     while not stopped:
+        # The first time the while loop iterates it completes the first part of
+        # the if clause
         if nxt_wrd == None:
-            # choose a random bi-gram
+            # choose a random bi-gram then find the value of said bi-gram. 
+            # Afterward choose a random value from the generated list
             rndm_bi_gram = random.choice(chains.keys())
-            # print rndm_bi_gram, "randomly choosen bi-gram"
+            print rndm_bi_gram, "randomly choosen bi-gram"
             rndm_bi_gram_value = chains[rndm_bi_gram]
             rndm_wrd = random.choice(rndm_bi_gram_value)
-            # print rndm_wrd, "first rndm word"
+            print rndm_wrd, "first rndm word"
 
             generated_txt = generated_txt +" "+ rndm_wrd
+            # the last word from the previous bi-gram key along with the randomly
+            # choosen word is saved for the next iteration of the while loop
             nxt_wrd = (rndm_bi_gram[1], rndm_wrd)
-            # print nxt_wrd, "first next word"
+            print nxt_wrd, "first next word"
         else:
+            # if the bi-gram from the previous while loop has a value, return it>
+            # If there is not a value, return None
             bi_gram_value = chains.get(nxt_wrd, None)
+
+            # if the bi-gram value is none, stop the while loop.
             if bi_gram_value == None:
                 stopped = True
+            # the random word is put in the first element of the nxt word tuple,
+            # and the second element is the newly choosen random word.
             else:
-                previous_rndm_wrd = rndm_wrd
+                previous_rndm_wrd = nxt_wrd[1]
                 new_rndm_wrd = random.choice(bi_gram_value)
-                # print new_rndm_wrd, "rnm word"
+                print new_rndm_wrd, "rnm word"
                 generated_txt = generated_txt +" "+ new_rndm_wrd
                 nxt_wrd = (previous_rndm_wrd, new_rndm_wrd)
-                # print nxt_wrd, "next word"
+                print nxt_wrd, "next word"
 
 
     return generated_txt
